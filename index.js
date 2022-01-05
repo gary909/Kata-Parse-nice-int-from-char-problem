2d5450ff9ca280245794013335bc9b87d0d01848
gary <gary.white@lodgit.com>
2022-01-05 13:26:03 +0100

    code working

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function getAge(inputString){
-    // return correct age (int). Happy coding :) 
+    let myStr = inputString.charAt(0); // return the 1st char
+    let myNum = parseInt(myStr); // convert char to num
+    return myNum; // return
 }
 
 console.log(getAge("4 years old")); // Return 4
